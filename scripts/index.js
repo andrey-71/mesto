841d2d8768ce369962ed56820011e71029570b87
Andrey Golovan <andrey.golovan71@yandex.ru>
2021-08-13 16:50:01 +0300

    style: подготовить проект к публикации

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,22 @@
-let container = document.querySelector(".content");
 // Попап
-let popup = container.querySelector(".popup");
-// Открыть - закрыть
-let popupOn = container.querySelector(".profile__edit-button");
-let popupOff = container.querySelector(".popup__close");
+let popup = document.querySelector(".popup");
+// Кнопки открыть/закрыть
+let popupOn = document.querySelector(".profile__edit-button");
+let popupOff = document.querySelector(".popup__close");
 // Поля с данными в профиле
-let nameProfile = container.querySelector(".profile__name");
-let jobProfile = container.querySelector(".profile__job");
+let nameProfile = document.querySelector(".profile__name");
+let jobProfile = document.querySelector(".profile__job");
 // Поля формы
-let nameInput = container.querySelector(".popup__name");
-let jobInput = container.querySelector(".popup__job");
-// Сохранить
-let saveButton = container.querySelector(".popup__save-button");
-
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__job");
+// Кнопка сохранить
+let saveButton = document.querySelector(".popup__save-button");
 
 // Открытие с записью/закрытие POPUP
 function togglePopup() {
   popup.classList.toggle("popup_active");
 
-  if (popup.classList.contains('popup_active')) {
+  if (popup.classList.contains("popup_active")) {
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
   }
@@ -37,13 +35,3 @@ function saveProfileData(evt) {
 }
 
 saveButton.addEventListener("click", saveProfileData);
-
-
-// Закрытие POPUP через ESC
-// container.addEventListener("keydown", function (e) {
-//   if (e.keyCode === 27) {
-//     togglePopup();
-//   }
-// });
-
-
