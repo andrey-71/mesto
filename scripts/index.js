d63ad92912a18a9554a7c1046b5ebcd3af2bca43
Andrey Golovan <andrey.golovan71@yandex.ru>
2021-08-13 14:10:29 +0300

    refactor: Оптимизировать структуру кода в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,49 @@
 let container = document.querySelector(".content");
 // Попап
-let popUp = container.querySelector(".popup");
-// Поля формы
-let nameInput = container.querySelector('.popup__name');
-let jobInput = container.querySelector('.popup__job');
-// Сохранить
-let saveButton = container.querySelector('.popup__save-button');
-// Поля с данными в профиле
-let nameProfile = container.querySelector('.profile__name');
-let jobProfile = container.querySelector('.profile__job');
+let popup = container.querySelector(".popup");
 // Открыть - закрыть
-let editButtonOn = container.querySelector(".profile__edit-button");
-let editButtonOff = container.querySelector(".popup__close");
+let popupOn = container.querySelector(".profile__edit-button");
+let popupOff = container.querySelector(".popup__close");
+// Поля с данными в профиле
+let nameProfile = container.querySelector(".profile__name");
+let jobProfile = container.querySelector(".profile__job");
+// Поля формы
+let nameInput = container.querySelector(".popup__name");
+let jobInput = container.querySelector(".popup__job");
+// Сохранить
+let saveButton = container.querySelector(".popup__save-button");
 
 
-// Отправка формы - ШО С ЭТИМ ДЕЛАТЬ?--------
-function formSubmitHandler(evt) {
+// Открытие с записью/закрытие POPUP
+function togglePopup() {
+  popup.classList.toggle("popup_active");
+
+  if (popup.classList.contains('popup_active')) {
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+  }
+}
+
+popupOn.addEventListener("click", togglePopup);
+popupOff.addEventListener("click", togglePopup);
+
+// Отправка формы с сохранением результата в профиль
+function saveProfileData(evt) {
   evt.preventDefault();
 
-}
-//-------------------------------------------
-
-
-popUp.addEventListener('submit', formSubmitHandler);
-
-// Открытие POPUP
-function editProfileOn() {
-  popUp.classList.add("popup_active");
-
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = jobProfile.textContent;
-}
-
-editButtonOn.addEventListener("click", editProfileOn);
-
-// Cохранение результата
-function saveEditProfile() {
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  editProfileOff();
+  togglePopup();
 }
 
-saveButton.addEventListener('click', saveEditProfile);
+saveButton.addEventListener("click", saveProfileData);
 
-// Закрытие POPUP
-function editProfileOff() {
-  popUp.classList.remove("popup_active");
-}
-
-editButtonOff.addEventListener("click", editProfileOff);
 
 // Закрытие POPUP через ESC
-container.addEventListener('keydown', function(e) {
-  if (e.keyCode === 27) {
-    editProfileOff();
-  }
-});
+// container.addEventListener("keydown", function (e) {
+//   if (e.keyCode === 27) {
+//     togglePopup();
+//   }
+// });
+
+
