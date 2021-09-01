e47f6596dc72ef6923c916ab1c4a14a5827d3413
andrey-71 <and_go1991@mail.ru>
2021-09-01 20:24:04 +0300

    refactor: вынести массив с карточками в отдельный .js файл

--- scripts/index.js
+++ scripts/index.js
@@ -47,33 +47,6 @@ const saveProfileData = (evt) => {
 // Обработчик события для формы редактирования профиля
 popupFormEditProfile.addEventListener("submit", saveProfileData);
 
-// Массив объектов-карточек
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 // Template
 const cardTemplate = document.querySelector("#card-template").content;
 // Контейнер с карточками
