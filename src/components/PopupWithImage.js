eaa0337c0ed833953f6c4ef5d8d8be914855af35
andrey-71 <and_go1991@mail.ru>
2021-11-03 01:33:08 +0300

    refactor: вынести поиск элементов карточки в конструктор класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,11 +1,14 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._cardImage = this._popup.querySelector('.popup__card-view-photo');
+    this._cardCaption = this._popup.querySelector('.popup__card-view-caption');
+  }
 
   // Открытие просмотра карточки с записью данных из карточки в окно просмотра
   open(cardElement) {
-    this._cardImage = this._popup.querySelector('.popup__card-view-photo');
-    this._cardCaption = this._popup.querySelector('.popup__card-view-caption');
     this._cardImage.src = cardElement.link;
     this._cardImage.alt = cardElement.name;
     this._cardCaption.textContent = cardElement.name;
